cf4a32a019c0b6337b9a7bd9fb365ff65570d9a3
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-03 07:05:10 -0300

    feat: create role migration

---
+++ prisma/migrations/20221203100500_create_role/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "roles" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_ad" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
+);
